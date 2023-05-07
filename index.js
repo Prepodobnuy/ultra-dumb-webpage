890828465268ad47f74ddd65ed1de46fb20e87a4
Bobr <130589949+Prepodobnuy@users.noreply.github.com>
2023-05-07 17:24:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function setLocation() {
+    function check(str) {
+        if (str != '') {
+            for (let i = 0; i < str.length; i++) {
+                if (str[i] !== ' ') {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    let val = ''
+    val += document.getElementById("inputs").value
+    if (check(val)) {
+        window.location.href = 'https://www.google.com/search?q=' + val
+    }
+}
+
+function favOpen(id) {
+    let a = [
+        'https://osu.ppy.sh/users/11978262',
+        'https://mail.google.com/mail/u/0/#inbox',
+        'https://www.youtube.com/'
+    ]
+    window.location.href = a[id]
+}
+
+function reimage() {
+    let arr = ['images/kitkat.png', 'images/osu.png', 'images/roflik.png']
+    let now = Math.floor(Math.random() * arr.length)
+    document.getElementById("myimg").src = arr[now]
+}
